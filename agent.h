daa80f71d196f6bec14154d394549041a39397e3
jasonchien1996 <43934912+jasonchien1996@users.noreply.github.com>
2018-11-14 14:14:19 +0800

    TD0 Learning
    TD0 Learning

--- agent.h
+++ agent.h
@@ -7,6 +7,9 @@
 #include <algorithm>
 #include "board.h"
 #include "action.h"
+#include "weight.h"
+#include <fstream>
+#include <math.h>
 
 class agent {
 public:
@@ -51,6 +54,65 @@ public:
 
 protected:
 	std::default_random_engine engine;
+};
+
+/**
+ * base agent for agents with weight tables
+ */
+class weight_agent : public agent {
+public:
+	weight_agent(const std::string& args = "") : agent(args) {
+		//if (meta.find("init") != meta.end()) // pass init=... to initialize the weight
+			init_weights(meta["init"]);
+		if (meta.find("load") != meta.end()) // pass load=... to load from a specific file
+			load_weights(meta["load"]);
+	}
+	virtual ~weight_agent() {
+		if (meta.find("save") != meta.end()) // pass save=... to save to a specific file
+			save_weights(meta["save"]);
+	}
+
+protected:
+	virtual void init_weights(const std::string& info) {
+		net.emplace_back(11390625); // create an empty weight table with size 65536
+		net.emplace_back(11390625); // create an empty weight table with size 65536
+		// now net.size() == 2; net[0].size() == 65536; net[1].size() == 65536
+	}
+	virtual void load_weights(const std::string& path) {
+		std::ifstream in(path, std::ios::in | std::ios::binary);
+		if (!in.is_open()) std::exit(-1);
+		uint32_t size;
+		in.read(reinterpret_cast<char*>(&size), sizeof(size));
+		net.resize(size);
+		for (weight& w : net) in >> w;
+		in.close();
+	}
+	virtual void save_weights(const std::string& path) {
+		std::ofstream out(path, std::ios::out | std::ios::binary | std::ios::trunc);
+		if (!out.is_open()) std::exit(-1);
+		uint32_t size = net.size();
+		out.write(reinterpret_cast<char*>(&size), sizeof(size));
+		for (weight& w : net) out << w;
+		out.close();
+	}
+
+protected:
+	std::vector<weight> net;
+};
+
+/**
+ * base agent for agents with a learning rate
+ */
+class learning_agent : public weight_agent {
+public:
+	learning_agent(const std::string& args = "") : weight_agent(args), alpha(0.1f) {
+		if (meta.find("alpha") != meta.end())
+			alpha = float(meta["alpha"]);
+	}
+	virtual ~learning_agent() {}
+
+protected:
+	float alpha;
 };
 
 /**
@@ -169,23 +231,157 @@ private:
 };
 
 /**
- * dummy player
- * select a legal action randomly
+ * td0 player
+ * select a legal action
  */
-class player : public random_agent {
+class player : public learning_agent {
 public:
-	player(const std::string& args = "") : random_agent("name=dummy role=player " + args),
-		opcode({ 0, 1, 2, 3 }) {}
+	player(const std::string& args = "") : learning_agent("name=learning role=player " + args) {}
 
-	virtual action take_action(const board& before) {
-		std::shuffle(opcode.begin(), opcode.end(), engine);
-		for (int op : opcode) {
-			board::reward reward = board(before).slide(op);
-			if (reward != -1) return action::slide(op);
-		}
-		return action();
-	}
+	virtual action take_action(const board &before)
+	{
+        int op, index = 0, valid = 0, t_r, reward;
+        float t = -999999, direction_value;
+        std::array<std::array<int, 32>, 4> key;
+        board as = before;
+        // find best action
+        for (int i = 0; i < 4; i++)
+        {
+            direction_value = 0;
+            as = before;
+            reward = as.slide(i); // r
+            //std::cout<<"reward"<<i<<" "<<reward[i]<<std::endl;
+            if (reward != -1)
+            {
+                valid = 1;
+                direction_value += reward;
+                for (int j = 0; j < 32; j++)
+                {
+                    for (int k = 0; k < 6; k++)//find index
+                    {
+                        int power = 1, n = 5-k;
+                        while(n--) power *=15;
+                        index = index + as.get_tile(pattern[j][k]) * power;
+                    }
+                    key[i][j] = index;
+                    if(j < 16)
+                        direction_value += net[0][index]; // v(as)
+                    else
+                        direction_value += net[1][index];
+                    index = 0;
+                }
+                if (direction_value > t)
+                {
+                    t = direction_value;
+                    op = i;
+                    t_r= reward;
+                }
+            }
+        }
+        //not terminal
+        if (valid == 1)
+        {
+            state_key.push_back(key[op]);
+            r.push_back(t_r);
+            return action::slide(op);
+        }
+        //terminal
+        for (int j = 0; j < 32; j++)
+        {
+            for (int k = 0; k < 6; k++)
+            {
+                int power = 1, n = 5-k;
+                while(n--) power *= 15;
+                index = index + as.get_tile(pattern[j][k]) * power;
+            }
+            key[0][j] = index;
+            index = 0;
+        }
+        state_key.push_back(key[0]);
+        r.push_back(0);
+        return action();
+    }
+
+    void training()
+    {
+        //training
+        std::array<int, 32> as_key;
+        float sum = 0, v_as, amend;
+        r.push_back(0);
+        while (!state_key.empty())
+        {
+            as_key = state_key.back();
+            v_as = sum;
+            sum = 0;
+            for (int j = 0; j < 32; j++)
+            {
+                if(j<16)
+                    sum += net[0][as_key[j]];
+                else
+                    sum += net[1][as_key[j]];
+            }
+            amend = alpha * (r.back() + v_as - sum) / 32;
+            sum = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if(i < 16)
+                {
+                    net[0][as_key[i]] += amend;
+                    sum += net[0][as_key[i]];
+                }
+                else
+                {
+                    net[1][as_key[i]] += amend;
+                    sum += net[1][as_key[i]];
+                }
+            }
+            state_key.pop_back();
+            r.pop_back();
+        }
+        r.pop_back();
+    }
 
 private:
-	std::array<int, 4> opcode;
+	std::vector<int> r;
+    std::vector<std::array<int, 32>> state_key;
+    const int pattern[32][6]={{ 0, 4, 8, 9,12,13},
+                        { 1, 5, 9,10,13,14},
+                        { 3, 2, 1, 5, 0, 4},
+                        { 7, 6, 5, 9, 4, 8},
+                        {14,10, 6, 5, 2, 1},
+                        {15,11, 7, 6, 3, 2},
+                        { 8, 9,10, 6,11, 7},
+                        {12,13,14,10,15,11},
+                        { 0, 1, 2, 6, 3, 7},
+                        { 4, 5, 6,10, 7,11},
+                        { 2, 6,10, 9,14,13},
+                        { 3, 7,11,10,15,14},
+                        {11,10, 9, 5, 8, 4},
+                        {15,14,13, 9,12, 8},
+                        {12, 8, 4, 5, 0, 1},
+                        {13, 9, 5, 6, 1, 2},//
+                        { 1, 2, 5, 6, 9,10},//
+                        { 2, 3, 6, 7,10,11},
+                        { 7,11, 6,10, 5, 9},
+                        {11,15,10,14, 9,13},
+                        {14,13,10, 9, 6, 5},
+                        {13,12, 9, 8, 5, 4},
+                        { 8, 4, 9, 5,10, 6},
+                        { 4, 0, 5, 1, 6, 2},
+                        {13,14, 9,10, 5, 6},
+                        {14,15,10,11, 6, 7},
+                        {11, 7,10, 6, 9, 5},
+                        { 7, 3, 6, 2, 5, 1},
+                        { 2, 1, 6, 5,10, 9},
+                        { 1, 0, 5, 4, 9, 8},
+                        { 4, 8, 5, 9, 6,10},
+                        { 8,12, 9,13,10,14}};
+    /*const int pattern[8][4] = {{0, 1, 2, 3},
+                        { 4, 5, 6, 7},
+                        { 8, 9,10,11},
+                        {12,13,14,15},
+                        { 0, 4, 8,12},
+                        { 1, 5, 9,13},
+                        { 2, 6,10,14},
+                        { 3, 7,11,15}};*/
 };
